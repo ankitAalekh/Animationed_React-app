9b8db5d4873db900323c29be2aa076bb9de6a6df
ankitAalekh <ankitbokaro1996@gmail.com>
2021-09-09 23:20:22 +0530

    Added css animation

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 import './Backdrop.css';
 
-const backdrop = (props) => (
-    <div className="Backdrop"></div>
-);
+const backdrop = (props) => {
+    const cssClass = ['Backdrop',props.show ? 'BackdropOpen' : 'BackdropClosed']; 
+    return <div className={cssClass.join(' ')}></div>
+};
 
 export default backdrop;
