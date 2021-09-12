2dd1d3ca8d333c9f5617381307596db359fd0dcb
ankitAalekh <ankitbokaro1996@gmail.com>
2021-09-12 17:27:38 +0530

    Adding animation with transition event

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react";
+import { Transition } from "react-transition-group";
 
-import './Modal.css';
+import "./Modal.css";
+
+const animationTiming ={
+  enter:400,
+  exit:1000
+}
 
 const modal = (props) => {
-    const cssClass = ['Modal',props.show ? 'ModalOpen' : 'ModalClosed']; 
-    return <div className={cssClass.join(' ')}>
-        <h1>A Modal</h1>
-        <button className="Button" onClick={props.closed}>Dismiss</button>
-    </div>
+  return (
+    <Transition mountOnEnter unmountOnExit in={props.show} timeout={animationTiming}>
+      {(state) => {
+        const cssClass = [
+          "Modal",
+          state === "entering"
+            ? "ModalOpen"
+            : state === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        return (
+          <div className={cssClass.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
+  );
 };
 
 export default modal;
